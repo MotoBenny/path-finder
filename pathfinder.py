bc492c134cd3d67966d1df7d12bf61fc2fb4ebb4
Benjamin <benj.james.carter@gmail.com>
2022-10-12 12:01:18 -0700

    added print maze

--- pathfinder.py
+++ pathfinder.py
@@ -18,14 +18,23 @@ maze = [
 ]
 
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)  # noqa: F841
+    RED = curses.color_pair(2)  # noqa: F841
+
+    for i, row in enumerate(maze):  # gives me index and value
+        for j, value in enumerate(row):
+            stdscr.addstr()
+
+
 def main(stdscr):  # standard output screen.
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
-    blue_white = curses.color_pair(1)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
 
-    stdscr.clear()
-    stdscr.addstr(0, 0, "Hello!", blue_white)  # row, column, message
-    stdscr.refresh()
-    stdscr.getch()  # waits for input before closing the terminal.
+    # stdscr.clear()
+    # stdscr.addstr(0, 0, "Hello!", blue_white)  # row, column, message
+    # stdscr.refresh()
+    # stdscr.getch()  # waits for input before closing the terminal.
 
 
 wrapper(main)
