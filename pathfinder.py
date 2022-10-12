4b185da853b7a9e750875f73866bacc5d4481e85
Benjamin <benj.james.carter@gmail.com>
2022-10-12 11:48:18 -0700

    added curses

--- pathfinder.py
+++ pathfinder.py
@@ -1,5 +1,6 @@
 # import curses
 from curses import wrapper
+import curses
 
 # import queue
 # import time
@@ -18,8 +19,11 @@ maze = [
 
 
 def main(stdscr):  # standard output screen.
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
+    blue_white = curses.color_pair(1)
+
     stdscr.clear()
-    stdscr.addstr(0, 0, "Hello!")
+    stdscr.addstr(0, 0, "Hello!", blue_white)  # row, column, message
     stdscr.refresh()
     stdscr.getch()  # waits for input before closing the terminal.
 
